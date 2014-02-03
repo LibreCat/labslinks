ff7f0493c1683ca053fc2dcfd5733805e10762f2
vpeil <vitali.peil@uni-bielefeld.de>
2014-02-03 15:14:33 +0100

    final considerations

--- createLinks.pl
+++ createLinks.pl
@@ -6,14 +6,14 @@ use Catmandu::Importer::EuropePMC;
 use YAML;
 
 Catmandu->load;
+Catmandu->config;
 
-my $bag      = Catmandu->store('search')->bag('publicationItem');
-my $importer = Catmandu->importer;
+my $importer = Catmandu->importer; #print Dump $importer->first;
 my $exporter = Catmandu->exporter;
 
 $importer->each(
     sub {
-        my $rec = $_;
+        my $rec = $_[0];
 
         my $importer =
           Catmandu::Importer::EuropePMC->new( query => $rec->{pmid} );
@@ -22,14 +22,14 @@ $importer->each(
         if ( $data->{hitCount} == 1
             && lc $data->{resultList}->{result}->{inEPMC} eq 'n' )
         {
-            $exporter->add( { pmid => $rec->{pmid}, id => $rec->{id} } );
+            $exporter->add( { conf => config, pmid => $rec->{pmid}, id => $rec->{id} } );
         }
         elsif ( $data->{hitCount} > 1 ) {
             foreach my $item ( @{ $data->{resultList}->{result} } ) {
                 if ( lc $item->{source} eq 'med' && lc $item->{inEPMC} eq 'n' )
                 {
                     $exporter->add(
-                        { pmid => $rec->{pmid}, id => $rec->{id} } );
+                        { conf => config, pmid => $rec->{pmid}, id => $rec->{id} } );
                 }
             }
         }
